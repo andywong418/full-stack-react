e8bc72f1911a2285e6b35fd0f838e79e6b5d2912
Andros <wong22@hotmail.co.uk>
2017-10-06 14:58:47 -0700

    Add toggling

--- reactApp/Components/ToDoItem.js
+++ reactApp/Components/ToDoItem.js
@@ -10,18 +10,37 @@ function renderItem(item, completed, index){
 function handleClick(props){
   props.onClick(props.index);
 }
-export default function ToDoItem(props) {
+export default class ToDoItem extends Component {
 
+  constructor(props){
+    super(props);
+    this.state = {
+      show: true
+    }
+    this.setToggle = this.setToggle.bind(this);
+  }
 
+  setToggle(){
+    console.log("HI")
+    this.setState({show: this.state.show ? false : true})
+  }
+  render(){
+    return(
 
-  return(
-    <div className="row">
-      <div className="col-sm-2">
-        {renderItem(props.item, props.completed, props.index)}
+      <div className="row">
+          <div className={this.state.show ? 'active-element' : 'hidden-element'}>
+        <div className="col-sm-2">
+          {renderItem(this.props.item, this.props.completed, this.props.index)}
+        </div>
+        </div>
+        <div className="col-sm-4">
+          <i onClick={() => { handleClick(this.props)}} key={this.props.index.toString()}  style={{color: 'red'}} className="fa fa-window-close" aria-hidden="true"></i>
+          <a onClick={this.setToggle} className="btn btn-default" style={{marginLeft: '10px'}}> {this.state.show ? "Hide" : "Show"}</a>
+        </div>
       </div>
-      <div className="col-sm-4">
-        <i onClick={() => { handleClick(props)}} key={props.index.toString()}  style={{color: 'red'}} className="fa fa-window-close" aria-hidden="true"></i>
-      </div>
-    </div>
-  )
+
+    )
+  }
+
+
 }
